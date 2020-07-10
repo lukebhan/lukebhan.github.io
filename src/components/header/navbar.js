f56c88acbc31185fd66ef1dd008fae78c6548a12
luke.bhan@vanderbilt.edu <luke.bhan@vanderbilt.edu>
2020-07-10 01:59:55 -0500

    update navbar

--- src/components/header/navbar.js
+++ src/components/header/navbar.js
@@ -4,7 +4,7 @@ import styles from "./header.module.scss";
 export default function Header(props) {
 	return (
 		<header className={styles.header}>
-			<a className = {styles.name} href={'about'}>
+			<a className = {styles.name} href={'../about'}>
 				Luke Bhan
 			</a>
 		<ul className={styles.headerMenu}>
